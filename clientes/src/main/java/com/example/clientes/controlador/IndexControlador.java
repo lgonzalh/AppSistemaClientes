f12dff2dd760808179c30d5909b8348793a5420e
Lucho <49911858+lgonzalh@users.noreply.github.com>
2024-04-30 00:03:18 -0500

    Update project Java

---
+++ clientes/src/main/java/com/example/clientes/controlador/IndexControlador.java
@@ -0,0 +1,93 @@
+package com.example.clientes.controlador;
+import com.example.clientes.modelo.Cliente;
+import com.example.clientes.servicio.ClienteServicio;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.control.*;
+
+@Component
+public class IndexControlador implements Initializable {
+    private static final Logger logger =
+            LoggerFactory.getLogger(IndexControlador.class);
+    @Autowired
+    private ClienteServicio clienteServicio;
+
+    @FXML
+    private TableView<Cliente> clienteTabla;
+
+    @FXML
+    private TableColumn<Cliente, Integer> idClienteColumna;
+
+    @FXML
+    private TableColumn<Cliente, String> nombreClienteColumna;
+
+    @FXML
+    private TableColumn<Cliente, String> domicilioClienteColumna;
+
+    @FXML
+    private TableColumn<Cliente, String> telefonoClienteColumna;
+
+    private final ObservableList<Cliente> clienteList =
+            FXCollections.observableArrayList();
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        clienteTabla.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        configurarColumnas();
+        listarClientes();
+
+    }
+    private void configurarColumnas(){
+        idClienteColumna.setCellValueFactory(new PropertyValueFactory<>("idCliente"));
+        nombreClienteColumna.setCellValueFactory(new PropertyValueFactory<>("nombreCliente"));
+        domicilioClienteColumna.setCellValueFactory(new PropertyValueFactory<>("domicilioCliente"));
+        telefonoClienteColumna.setCellValueFactory(new PropertyValueFactory<>("telefonoCliente"));
+    }
+    private void listarClientes(){
+        logger.info("Ejecutando listado de clientes");
+        clienteList.clear();
+        clienteList.addAll(clienteServicio.ListarClientes());
+        clienteTabla.setItems(clienteList);
+    }
+
+
+/*
+    private void recolectarDatosFormulario(Cliente cliente){
+        //-----Parte 2----
+        if(idClienteInterno!=null)
+            cliente.setIdCliente(idClienteInterno);
+        //------------------------------------Pase a modificar agregarCliente
+
+        cliente.setNombreCliente(txtNombre.getText());
+        cliente.setDomicilioCliente(txtDomicilio.getText());
+        cliente.setTelefonoCliente(txtTelefono.getText());
+    }
+
+    private void limpiarFormulario(){
+
+        txtNombre.clear();
+        txtDomicilio.clear();
+        txtTelefono.clear();
+    }
+
+    private void mostrarMensaje(String titulo, String mensaje){
+        Alert alerta=new Alert(Alert.AlertType.INFORMATION);
+        alerta.setTitle(titulo);
+        alerta.setHeaderText(null);
+        alerta.setContentText(mensaje);
+        alerta.showAndWait();
+    }
+*/
+
+}
---
+++ clientes/src/main/java/com/example/clientes/presentacion/SistemaClientesFx.java
@@ -0,0 +1,39 @@
+package com.example.clientes.presentacion;
+import  com.example.clientes.ClientesApplication;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class SistemaClientesFx extends Application {
+    private ConfigurableApplicationContext applicationContext;
+
+    //public static void main(String[] args) {
+    // launch(args);
+    //}
+    @Override
+    public void init(){
+        this.applicationContext =
+                new SpringApplicationBuilder(ClientesApplication.class).run();
+    }
+    @Override
+    public void start(Stage stage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(ClientesApplication.class.getResource("/templates/index.fxml"));
+        loader.setControllerFactory(applicationContext::getBean);
+        Scene escena = new Scene(loader.load());
+        Image icon = new Image(getClass().getResourceAsStream("/favicon.ico"));
+        stage.getIcons().add(icon);
+        stage.setScene(escena);
+        stage.show();
+
+    }
+    @Override
+    public void stop(){
+        applicationContext.close();
+        Platform.exit();
+    }
+}
